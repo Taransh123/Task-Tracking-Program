a4ceeb5010395b3143e8b25f84854f4d347db65c
Taransh Goyal <taransh.goyal@gmail.com>
2025-02-03 00:17:29 -0500

    Delete Main.java

--- Main.java
+++
@@ -1,7 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-        TaskManager taskManager = new TaskManager();
-        ConsoleUI ui = new ConsoleUI(taskManager);
-        ui.start();
-    }
-}
